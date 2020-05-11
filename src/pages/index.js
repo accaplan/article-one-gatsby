e9a0f835444d04cdbc14a1205c5aaff6e965ddcb
Lucas Vocos <lucasvocos@gmail.com>
2020-05-11 14:53:52 -0400

    begin styling grid

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,51 @@
 import React, { useState, useEffect } from "react"
-import { useStaticQuery, graphql } from "gatsby"
+import { useStaticQuery, graphql, Link } from "gatsby"
+import Img from "gatsby-image"
 import Layout from "../components/Layout/Layout"
 import SEO from "../components/seo"
+import styled from "styled-components"
+
+const PostsWrapper = styled.main`
+  display: flex;
+  flex-wrap: wrap;
+`
+
+const PostLink = styled.article`
+  width: 100%;
+  box-sizing: border-box;
+  padding: 20px;
+
+  @media (min-width: 1024px) {
+    &:nth-of-type(5n + 1),
+    &:nth-of-type(5n + 2) {
+      width: 50%;
+      border-bottom: 1px solid;
+    }
+    &:nth-of-type(5n + 3),
+    &:nth-of-type(5n + 4),
+    &:nth-of-type(5n + 5) {
+      width: 33.3%;
+      border-bottom: 1px solid;
+    }
+    &:nth-of-type(5n + 1) {
+      border-right: 1px solid;
+    }
+    &:nth-of-type(5n + 3),
+    &:nth-of-type(5n + 4) {
+      border-right: 1px solid;
+    }
+  }
+`
+
+const Thumbnail = styled.section``
+
+const PostInfo = styled.section``
+
+const Title = styled.h2``
+
+const Subtitle = styled.h3``
+
+const PostCategory = styled.p``
 
 const IndexPage = () => {
   const data = useStaticQuery(graphql`
@@ -47,13 +91,33 @@ const IndexPage = () => {
     data.allSanityCategory.nodes.concat({ title: "All" })
   )
   const [currentCategory] = useState({ title: "All" })
-  const [currentPosts] = useState(null)
+  const [currentPosts] = useState(data.allSanityPost.nodes)
 
-  debugger
+  // debugger
 
   return (
     <Layout>
       <SEO title="Home" />
+
+      <PostsWrapper>
+        {currentPosts &&
+          currentPosts.map(post => {
+            return (
+              <PostLink key={post.id}>
+                <Link to={`/${post.slug.current}`}>
+                  <Thumbnail>
+                    <Img fluid={post.thumbnail.asset.fluid} />
+                  </Thumbnail>
+                  <PostInfo>
+                    <Title>{post.title}</Title>
+                    <Subtitle>{post.subhead}</Subtitle>
+                    <PostCategory>{post.category[0].title}</PostCategory>
+                  </PostInfo>
+                </Link>
+              </PostLink>
+            )
+          })}
+      </PostsWrapper>
     </Layout>
   )
 }
